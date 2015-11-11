f8f9b201b55a3c03d7a322da7dff1d2fbb476b5e
Lance <Lance@thelpstory.com>
2015-11-11 09:55:46 -0700

    all tests passing

--- js/main.js
+++ js/main.js
@@ -1,3 +1,16 @@
-// add scripts
+var units = ['B', 'KB', 'MB', 'GB', 'TB', 'PB', 'EB', 'ZB', 'YB']
 
-console.log("sanity check!");
+function byteString(num) {
+    var index = 0
+    while (num > 1024) {
+
+        if (index === 8) {
+            return num.toFixed(2) + ' ' + units[8];
+        }
+
+        num = num / 1024;
+        index++;
+    }
+
+    return num.toFixed(2) + ' ' + units[index];
+}
